a80b32f982e2947488a1fd9243abffd9afa8a004
Lee Scotter <scotter77@mailfence.com>
2021-03-12 00:32:38 +0000

    Minor compile fixes

--- drivers/regulator/cpr3-regulator.c
+++ drivers/regulator/cpr3-regulator.c
@@ -6178,6 +6178,7 @@ int cpr3_regulator_unregister(struct cpr3_controller *ctrl)
 		if (rc) {
 			cpr3_err(ctrl, "failed to clear CPR4 configuration,rc=%d\n",
 				rc);
+        }
 	}
 
 	cpr3_ctrl_loop_disable(ctrl);
